32228f9f9e7e804dd84845977520546f9c5f8151
Sravanikoppisetti <koppisettisravs@msitprogram.net>
2022-11-15 14:07:42 +0530

    contentadmin

--- lms/content/admin.py
+++ lms/content/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
 # Register your models here....
-from . models import Signup
+from . models import Signup,Courses,Chapters,Lesson,Assignmenttopic,MCQtopic,lessncontent,mcq
 admin.site.register(Signup)
+admin.site.register(Courses)
+admin.site.register(Chapters)
+admin.site.register(Lesson)
+admin.site.register(Assignmenttopic)
+admin.site.register(MCQtopic)
+admin.site.register(lessncontent)
+admin.site.register(mcq)
