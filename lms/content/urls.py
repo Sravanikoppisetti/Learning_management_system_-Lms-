a20280bb6fb798dfecdfd3d4db3759c79c81aabb
Sravanikoppisetti <koppisettisravs@msitprogram.net>
2022-11-15 14:34:52 +0530

    contenturls

--- lms/content/urls.py
+++ lms/content/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
    path('',views.signin, name="login"),
    path('signup/',views.signup, name="signup"),
    path('logged/',views.logged, name="logged"),
-   path('register/',views.register, name="register")
+   path('register/',views.register, name="register"),
+   path('course', views.courses),
+   path("coursesearch", views.search),
+   path("<int:course_id>", views.coursecontent, name="coursecontent"),
+   path("<str:content>", views.contentdisplay, name="contentdispaly") 
 ]
