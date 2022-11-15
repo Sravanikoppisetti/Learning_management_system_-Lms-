8bccd95a8f9ba410fa5ce852a4f7a7b3e1d513ca
Sravanikoppisetti <koppisettisravs@msitprogram.net>
2022-11-15 13:50:56 +0530

    contentmodels

--- lms/content/models.py
+++ lms/content/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime, date
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 
@@ -14,3 +15,86 @@ class Signup(models.Model):
 
     def __str__(self):
         return self.user_name
+
+
+class Courses(models.Model):
+    courseid=models.IntegerField(primary_key=True)
+    coursename=models.CharField(max_length=100,blank=True, default="draftname")
+    coursecredits=models.IntegerField(blank=True, default=0)
+    courseimg=models.CharField(max_length=100)
+
+    def __str__(self):
+        return  self.coursename
+    
+   
+
+class Chapters(models.Model):
+    chapterid=models.IntegerField(primary_key=True)
+    chaptername=models.CharField(max_length=100,blank=True, default="draftchaptername")
+    course=models.ForeignKey(Courses,on_delete=models.CASCADE )
+
+    
+
+    def __str__(self):
+        return self.chaptername
+
+   
+
+class Lesson(models.Model):
+    lessonid=models.IntegerField(primary_key=True)
+    lessonname=models.CharField(max_length=100,blank=True, default="Lessonname")
+    chapter=models.ForeignKey(Chapters, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.lessonname
+
+
+class Assignmenttopic(models.Model):
+    Assignmenttopicid=models.IntegerField(primary_key=True)
+    Assignmnetname=models.CharField(max_length=100,blank=True, default="Lessonname")
+    chapter=models.ForeignKey(Chapters, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.Assignmnetname
+
+class MCQtopic(models.Model):
+    MCQtopicid=models.IntegerField(primary_key=True)
+    MCQtopicname=models.CharField(max_length=100,blank=True, default="Lessonname")
+    chapter=models.ForeignKey(Chapters, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.MCQtopicname
+
+
+
+
+
+
+class lessncontent(models.Model):
+    contentid=models.IntegerField(primary_key=True)
+    video=models.CharField(max_length=100,blank=True, default="")
+    text= RichTextField(blank=True,null=True)
+    lesson=models.ForeignKey(Lesson, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return  self.video
+    
+class mcq(models.Model):
+    mcqid=models.IntegerField(primary_key=True)
+    question= RichTextField(blank=True,null=True)
+    option1=RichTextField(blank=True,null=True)
+    opt1answ=models.BooleanField(default=False)
+    option2=RichTextField(blank=True,null=True)
+    opt2answ=models.BooleanField(default=False)
+    option3=RichTextField(blank=True,null=True)
+    opt3answ=models.BooleanField(default=False)
+    option4=RichTextField(blank=True,null=True)
+    opt4answ=models.BooleanField(default=False)
+    lesson=models.ForeignKey(Lesson, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return  self.question
+    
