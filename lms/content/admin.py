59cedf0d809c248bf4ef15661992d02d5702d0ed
Sravanikoppisetti <koppisettisravs@msitprogram.net>
2022-11-19 12:37:36 +0530

    added mcq&assignmenst

--- lms/content/admin.py
+++ lms/content/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here....
-from . models import Signup,Courses,Chapters,Lesson,Assignmenttopic,MCQtopic,lessncontent,mcq
+from . models import Signup,Courses,Chapters,Lesson,Assignmenttopic,MCQtopic,lessncontent,mcqansw,assignmenttask
 admin.site.register(Signup)
 admin.site.register(Courses)
 admin.site.register(Chapters)
@@ -9,4 +9,5 @@ admin.site.register(Lesson)
 admin.site.register(Assignmenttopic)
 admin.site.register(MCQtopic)
 admin.site.register(lessncontent)
-admin.site.register(mcq)
+admin.site.register(mcqansw)
+admin.site.register(assignmenttask)
