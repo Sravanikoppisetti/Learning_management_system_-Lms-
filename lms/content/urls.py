bed95592bee365ccb1127177dbf9086481f97377
Sravanikoppisetti <koppisettisravs@msitprogram.net>
2022-11-24 14:29:55 +0530

    imagesadded

--- lms/content/urls.py
+++ lms/content/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
    path('signup/',views.signup, name="signup"),
    path('logged/',views.logged, name="logged"),
    path('register/',views.register, name="register"),
-   path('course', views.courses, name="course"),
+   path('course/', views.courses, name="course"),
    path("coursesearch", views.search),
    path("<int:course_id>", views.coursecontent, name="coursecontent"),
    path("<int:courseid>/<int:chapid>/<str:content>/", views.contentdisplay, name="contentdispaly"),
